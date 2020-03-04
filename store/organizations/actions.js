f86c8c2bed9e6b9082d5366e591ece522204054c
JamesSCBD <jbarnard@kersonwallaw.com>
2020-03-04 18:05:44 +0530

    Code refactor

--- store/organizations/actions.js
+++ store/organizations/actions.js
@@ -1,7 +1,6 @@
 export async function getDataFromApi ({ commit, rootState }){
   const organizations  = await this.$kronosApi.getOrganizationTypes()
 
-  debugger
   commit('set', organizations)
   return organizations
 }
