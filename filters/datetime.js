ceabf952ea3aef90bf9a6c1b761ef33febe58dce
JamesSCBD <60912214+JamesSCBD@users.noreply.github.com>
2020-05-05 21:13:44 +0530

    Use luxon instead of moment

--- filters/datetime.js
+++ filters/datetime.js
@@ -1,15 +1,26 @@
-import moment from 'moment';
+import { DateTime } from 'luxon';
 
 function datetime(value) {
-  return value ? moment(value).format('YYYY-MM-DD HH:mm') : '';
+  return toDateFormat(value, 'yyyy-MM-dd hh:mm');
 }
 
 function date(value) {
-  return value ? moment(value).format('YYYY-MM-DD') : '';
+  return toDateFormat(value, 'yyyy-MM-dd');
 }
 
 function time(value) {
-  return value ? moment(value).format('HH:mm') : '';
+  return toDateFormat(value, 'hh:mm');
+}
+
+function toDateFormat(value, format) {
+  let dateValue = null;
+  if (value) {
+    if (DateTime.isDateTime(value)) dateValue = value;
+    if (value.constructor === Date) dateValue = DateTime.fromJSDate(value);
+    if (value.constructor === String) dateValue = DateTime.fromISO(value);
+  }
+
+  return dateValue ? dateValue.toFormat(format) : '';
 }
 
 export { datetime, date, time };
